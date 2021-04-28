3eb052814fbdcae594800e90f665566e88ead922
Максим <maksut69@mail.ru>
2021-04-28 16:37:54 +0300

    lesson1 on Kotlin is ready

---
+++ app/src/main/java/ru/m/kotlin_1/Note.kt
@@ -0,0 +1,6 @@
+package ru.m.kotlin_1
+
+data class Note(val note: String, val day: String)
+
+
+
---
+++ app/src/main/java/ru/m/kotlin_1/SomeCopy.kt
@@ -0,0 +1,9 @@
+package ru.m.kotlin_1
+
+object SomeCopy {
+    //val bred: String = "Object for fun!"
+
+    val one = Note("Great!", "Monday!")
+    val two = one.copy()
+
+}
